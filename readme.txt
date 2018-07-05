3f81c7c1ec5fcd56789db466c016a5b97805f4b6
lisongping <pingsoli@163.com>
2018-07-05 20:45:18 +0800

    delete all files

--- readme.txt
+++
@@ -1,4 +0,0 @@
-This file is user for testing
-
-add something from local directory workspace
-change something from github
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#define DEBUG 1
-
-int main(int argc, char** argv)
-{
-  int debug = DEBUG;
-  return 0;
-}
